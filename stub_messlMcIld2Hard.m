1a6c8afa3822dbbc4f559c93b6e8cae07e9a8155
Michael Mandel <mim@mr-pc.org>
2015-06-05 16:55:48 -0400

    able to use reference mic for messl multichannel stub

--- stub_messlMcIld2Hard.m
+++ stub_messlMcIld2Hard.m
@@ -1,7 +1,9 @@
-function Y = stub_messlMcIld2Hard(X, N, Ncov, fail, TDOA, fs, varargin)
+function Y = stub_messlMcIld2Hard(X, N, Ncov, fail, TDOA, fs, allPairs, varargin)
 
 % Multichannel MESSL mask applied to MVDR output initialized from ILD
 % between MVDR output and mic 2 (rear-facing)
+
+if ~exist('allPairs', 'var') || isempty(allPairs), allPairs = true; end
 
 % Check that mrfHardCompatExp is not zero
 ind = find(strcmp(varargin, 'mrfHardCompatExp'));
@@ -25,14 +27,28 @@ mvdr = stub_baselineMvdr(X, N, Ncov, fail, TDOA, fs);
 
 if ~fail(2)
     % Compute ILD between MVDR output and mic 2 (rear-facing)
-    maskInit = maxSup + (1-maxSup)*(db(magSq(mvdr(2:end-1,:)) ./ magSq(X(2:end-1,:,2))) > threshold_db);
+    maskInit = maxSup + (1-2*maxSup)*(db(magSq(mvdr(2:end-1,:)) ./ magSq(X(2:end-1,:,2))) > threshold_db);
     maskInit = cat(3, maskInit, 1 - maskInit);
 else
     maskInit = [];
 end
 
+% Figure out which mic to use for reference
+if allPairs
+    refMic = 0;
+else
+    if ~fail(2)
+        refMic = 2;
+    else
+        refMic = find(~fail, 1, 'first');
+    end
+    if isempty(refMic)
+        error('All potential reference mics have failed')
+    end
+end
+
 % MESSL for mask
-messlOpts = [{'GarbageSrc', 1, 'fixIPriors', 1, 'maskInit', maskInit} varargin];
+messlOpts = [{'GarbageSrc', 1, 'fixIPriors', 1, 'maskInit', maskInit, 'refMic', refMic} varargin];
 [p_lr_iwt params hardMasks] = messlMultichannel(X(2:end-1,:,~fail), tau, 1, messlOpts{:});
 
 z = zeros(1, size(X,2));
