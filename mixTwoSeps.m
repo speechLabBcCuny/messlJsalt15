169278c5b6ffff8f5f7a1370d25e3294b1ceb71f
Michael Mandel <mim@mr-pc.org>
2015-07-20 12:31:27 -0700

    mix two separations to make a third at varying gains

---
+++ mixTwoSeps.m
@@ -0,0 +1,42 @@
+function mixTwoSeps(inDirA, inDirB, outDir, gainA, gainB, overwrite, files)
+
+if ~exist('overwrite', 'var') || isempty(overwrite), overwrite = ...
+        false; end
+if ~exist('files', 'var'), files = []; end
+
+if isempty(files)
+    filesA = findFiles(inDirA, '.*.wav');
+    filesB = findFiles(inDirB, '.*.wav');
+    files = intersect(filesA, filesB);
+end
+
+for f = 1:length(files)
+    inFileA = fullfile(inDirA, files{f});
+    inFileB = fullfile(inDirB, files{f});
+    outFile = fullfile(outDir, files{f});
+    fprintf('%d: %s\n', f, files{f});
+
+    if exist(outFile, 'file') && ~overwrite,
+        fprintf('\b <-- Skipping\n');
+        continue;
+    end
+
+    [xa fsa] = wavread(inFileA);
+    [xb fsb] = wavread(inFileB);
+    assert(fsa == fsb);
+    fs = fsa;
+
+    % Find two peaks in ILD histogram corresponding to mask = {min,
+    % 1}, normalize so that mask = 1 means ILD 0 between Xa and Xb.
+    Xa = stft(xa', 1024, 1024, 256);
+    Xb = stft(xb', 1024, 1024, 256);
+    ild = db(Xa ./ Xb);
+    [h xh] = hist(ild(:), 1000);
+    p = pickPeaks(xh, h, 2);
+    xa = xa * 10^(-p(2)/20);
+
+    % Mix and save
+    x = (gainA * xa + gainB * xb) / (gainA + gainB);
+    ensureDirExists(outFile);
+    wavwrite(x, fs, outFile);
+end
