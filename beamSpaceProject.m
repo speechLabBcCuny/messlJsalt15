d5beb92270587f79e74fc541a4d1d87a141ff115
Michael Mandel <mim@mr-pc.org>
2015-07-21 09:45:38 -0700

    factored out beamformer

--- beamSpaceProject.m
+++ beamSpaceProject.m
@@ -8,22 +8,29 @@ taug = tauGrid(d_m, fs, 31);
 wlen = 2*(F-1);
 
 channelPairs = nchoosek(1:Ch, 2);
-tdoas = 
+tdoas = zeros(length(taug), T, size(channelPairs,1));
 for cc = 1:size(channelPairs,1)
-    T(:,:,cc) = tdoaOverTime(Y(:,:,channelPairs(cc,:)), taug);
+    tdoas(:,:,cc) = tdoaOverTime(Y(:,:,channelPairs(cc,:)), taug);
 end
-[~,perPair] = max(T,[],1);
+[~,perPair] = max(tdoas,[],1);
 perPair = taug(squeeze(perPair).');
 [perMic estPerPair] = perMicTdoaLs(perPair, channelPairs,[],0);
 
-[~,~,index]=kmedoids(estPerPair,K);
+[~,~,index] = kmedoids(estPerPair,K);
 
 Ybf = zeros(size(Y,1),size(Y,2),K);
 for kk=1:K
-    for ff = 1:F
-        Df = sqrt(1/Ch) * exp(-2*1i*pi*(ff-1)/wlen*perMic(:,index(kk))); % steering vector
-        Ybf(ff,:,kk) = (Df'*(squeeze(Y(ff,:,:)).')).';
-    end
+    Ybf(:,:,kk) = delayAndSum(Y, perMic(:,index(kk)), F, Ch, wlen);
 end
 
 Ybfn = bsxfun(@rdivide, Ybf, sqrt(sum(magSq(Ybf),3)));
+
+
+
+function Ybf = delayAndSum(Y, tdoas, F, Ch, wlen)
+
+Ybf = zeros(size(Y,1), size(Y,2));
+for ff = 1:F
+    Df = sqrt(1/Ch) * exp(2*1i*pi*(ff-1)/wlen*tdoas); % steering vector
+    Ybf(ff,:) = (Df'*(squeeze(Y(ff,:,:)).')).';
+end
