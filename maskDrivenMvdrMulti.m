b6cf9aae56a95163a3e3e8d862581386acd805c7
Michael Mandel <mim@mr-pc.org>
2015-08-05 17:40:19 -0700

    enforce that at least a certain number of frames must be off in MVDR mask at beginning and end for estimating noise covariances

--- maskDrivenMvdrMulti.m
+++ maskDrivenMvdrMulti.m
@@ -4,13 +4,16 @@ function [Xp mvdrMask mask] = maskDrivenMvdrMulti(X, mask, fail, tdoas)
 % functions. Outputs twice as many channels as sources, the first
 % set are MVDR+mask, the second set are MVDR only.
 
-mvdrMask = mungeMaskForMvdr(mask);
+minNoiseObsPerMic = 3;
+minNoiseObs = minNoiseObsPerMic * size(X,3);
+
 Xp = zeros(size(X,1), size(X,2), size(mask,3));
 for s = 1:size(mask,3)
     if s > size(tdoas,2)
         % No MVDR, pick a random channel
         Xp(:,:,s) = X(:,:,1);
     else
+        mvdrMask(:,:,s) = mungeMaskForMvdr(mask(:,:,s), minNoiseObs);
         Xp(:,:,s) = maskDrivenMvdr(X(:,:,~fail), mvdrMask(:,:,s), tdoas(:,s));
     end
 end
--- mungeMaskForMvdr.m
+++ mungeMaskForMvdr.m
@@ -1,9 +1,16 @@
-function mOut = mungeMaskForMvdr(mIn, minOnThresh, minOffThresh)
+function mOut = mungeMaskForMvdr(mIn, minOffPts, minOnThresh, minOffThresh)
 
 % Munge a separation mask to work well for MVDR noise estimation control
 
+if ~exist('minOffPts', 'var') || isempty(minOffPts), minOffPts = 0; end
 if ~exist('minOnThresh', 'var') || isempty(minOnThresh), minOnThresh = 0.35; end
 if ~exist('minOffThresh', 'var') || isempty(minOffThresh), minOffThresh = 0; end
+
+% Turn off mask at beginning and end (twice as long at the end) to get at
+% least minOffPts points where the MVDR mask is off to avoid NaNs in
+% spatial covariance.
+mIn(:,1:floor(minOffPts/3)) = 0;
+mIn(:,end-ceil(minOffPts*2/3)+1:end) = 0;
 
 fracOff = mean(1-mIn, 1);
 fracOn  = mean(  mIn, 1);
