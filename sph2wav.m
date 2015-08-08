37e0a5039a05fc7af8886cbfff4d97bc50517cac
Michael Mandel <mim@mr-pc.org>
2015-08-08 06:22:03 -0700

    updated kaldi path

--- sph2wav.m
+++ sph2wav.m
@@ -7,7 +7,7 @@ function [x fs] = sph2wav(src_file)
 if ~exist('cleanUp', 'var') || isempty(cleanUp), cleanUp = true; end
 
 % Set your own kaldi directory...
-kaldiDir = '/home/mmandel/code/github/kaldi-jsalt/tools/sph2pipe_v2.5';
+kaldiDir = '/home/mmandel/code/githubRO/kaldi-jsalt/tools/sph2pipe_v2.5';
 dest_file = [tempname '.wav'];
 
 [pathstr name ext] = fileparts(src_file);
