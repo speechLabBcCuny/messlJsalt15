b6dc13b25684798c0fff555f26332bcbf8ec6df9
Michael Mandel <mim@mr-pc.org>
2016-01-08 17:17:42 -0500

    made plotMultichanIldIpdStats accept an stft matrix or a filename

--- plotMultichanIldIpdStats.m
+++ plotMultichanIldIpdStats.m
@@ -1,11 +1,17 @@
-function plotMultichanIldIpdStats(wavFile)
+function plotMultichanIldIpdStats(wavFile_or_X)
 
 % Plot interaural level and phase difference stats for all pairs of mics
 
 wlen = 1024;
 
-[x fs] = wavread(wavFile);
-X = stft_multi(x.', wlen);
+if isstr(wavFile_or_X)
+    wavFile = wavFile_or_X;
+    [x fs] = wavread(wavFile);
+    X = stft_multi(x.', wlen);
+else
+    X = wavFile_or_X;
+end
+    
 %tau = tauGrid(0.35, fs, 31);
 tau = 0;
 
@@ -19,6 +25,13 @@ for c = 1:Np
     names{c} = sprintf('%d-%d', cp);
 end
 figure(1)
-subplots(A, [], names, @(r,c,i) caxis([-20 20]))
+subplots(A, [1 -1], names, @(r,c,i) caxis([-20 20]))
 figure(2)
-subplots(angE, [], names, @(r,c,i) colormap(hsv(254)))
+subplots(angE, [1 -1], names, @(r,c,i) colormap(hsv(254)))
+
+for c = 1:Ch
+    chNames{c} = sprintf('%d', c);
+end
+
+figure(3)
+subplots(cellFrom3D(db(X)), [1 -1], chNames, @(r,c,i) caxis([-100 0]))
