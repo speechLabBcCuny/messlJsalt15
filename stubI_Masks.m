b0e12f24a8a7838807af0a1bd35e166ec0a3f69e
nateanl <nizhaoheng@gmail.com>
2016-09-30 16:57:57 -0400

    fix stubI_Masks

--- stubI_Masks.m
+++ stubI_Masks.m
@@ -1,10 +1,19 @@
-function [Y data] = stubI_Masks(X, C, mode, fail, fs, inFile, I, refMic, d, useHardMask, beamformer, varargin)
-    C2 = repmat(C,1,1,1);
+function [Y data mask Xp] = stubI_Masks(X, fail, fs, inFile, mode, I, refMic, d, useHardMask, beamformer, varargin)
+    clean = audioread('/Users/Near/Desktop/MESSL/mvdr_test/dev2/output/wav/F01_22GC010A_BTH.wav');
+    C = stft_multi(clean.',1024);
+    C = C(:,:,1);
+    C2 = repmat(C,1,1,7);
     switch mode
         case 'ideal_amplitude'
-            data.mask = abs(X)/abs(C2);
+            data.mask = abs(X)./abs(C2);
         case 'phase_sensitive'
-            data.mask = real(X/C2);
-        case 'ideal_copmlex'
-            data.mask = X/C;
+            data.mask = real(X./C2);
+        case 'ideal_complex'
+            data.mask = X./C2;
     end
+    Xp = zeros(size(X,1), size(X,2), size(data.mask,3));
+for s = 1:size(data.mask,3)
+        % No MVDR, pick a random channel
+        Xp(:,:,s) = X(:,:,1);
+end
+Y = Xp .* data.mask;
