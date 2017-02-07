040216e10bafc08571f61ad18640f73310970c2e
nateanl <nizhaoheng@gmail.com>
2017-02-07 17:44:29 -0500

    fix bugs for test_mask

--- test_mask.py
+++ test_mask.py
@@ -3,12 +3,13 @@ import scipy.io as sio
 import numpy as np
 import keras.backend as K
 from keras.models import load_model
+import os
 
 base_dir = '/scratch/near/test/mask/'
 model_dir = '/home/data/CHiME3/experiments/exp1/exp_2017-02-03_00:38:48/'
 save_dir = '/scratch/near/test_prediction/'
 data_dir = '/home/data/CHiME3/data/audio/16kHz/local/messl-spects-noisy/data/'
-test_list = mk.prep_list_for_keras(data_dir, 'dt05_*_simu')
+test_list = mk.prep_list_for_keras(data_dir, 'dt05.*simu')
 print len(test_list)
 #load the latest model
 
@@ -19,12 +20,16 @@ print "......................"
 print "start predicting...."
 for test_file in test_list:
     noisy = sio.loadmat(test_file)['data'][0][0][0]
-    noisy = noisy.swapaxis(0,2)
+    noisy = noisy.swapaxes(0,2)
     test_input = abs(noisy)
     test_output = model.predict(test_input)
     #combine the six masks to one single mask by taking the average.
     mask = np.mean(test_output,axis=0)
     #save the mask to mat file
-    save_dir = test_file.replace("messl-spects-noisy", "lstm-predicted-mask")
-    sio.savemat(save_dir, {'mask': mask})
+    save_file = model_dir+test_file.replace(data_dir, "")
+    save_dir = os.path.split(save_file)[0]
+    if not os.path.exists(save_dir):
+        os.mkdir(save_dir)
+    sio.savemat(save_file, {'mask': mask})
 print "finish"
+print len(test_list)
