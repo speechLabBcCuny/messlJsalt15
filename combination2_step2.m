f31bf909217425989bc69a91ad9d08cae98ab87b
nateanl <nizhaoheng@gmail.com>
2017-03-04 16:05:33 -0500

    add replaymessl for c3 and prediction for c4

--- combination2_step2.m
+++ combination2_step2.m
@@ -3,10 +3,10 @@ addpath('../messl');
 addpath('../utils');
 workDir = '/home/data/CHiME3/data/audio/16kHz/isolated/';
 outDir = '/scratch/near/CHiME3/replayMessl/min/';
-combined_mask_dir = '/scratch/near/CHiME3/lstm_messl/min/data/';
+combined_mask_dir = '/scratch/near/CHiME3/v2/lstm_messl/min/data/';
 
 %put combined mask to MESSL
 enhance_wrapper(@(X, fail, fs, file) stubI_replayMessl(X, fail, fs, file, ...
   combined_mask_dir, '', 'souden', '', 'mask', 1), ...
   workDir, ...
-		outDir, [1 1], 0, 1, 1,'[de]t05.*\.CH1\.wav$'); 
+		outDir, [5 5], 1, 1, 1,'[de]t05((?!bth).)*\.CH1\.wav$'); 
