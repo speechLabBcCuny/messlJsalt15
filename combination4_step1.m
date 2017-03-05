179b64a289dce6a598af7b1cbd9f29b59cfaa4e0
nateanl <nizhaoheng@gmail.com>
2017-03-04 20:42:55 -0500

    replay messl for c4

---
+++ combination4_step1.m
@@ -0,0 +1,13 @@
+addpath('../mimlib');
+addpath('../messl');
+addpath('../utils');
+workDir = '/home/data/CHiME3/data/audio/16kHz/isolated/';
+outDir = '/scratch/near/CHiME3/v4/';
+combined_mask_dir = '/scratch/near/CHiME3/lstm_outpt_c3/';
+
+%put combined mask to MESSL
+enhance_wrapper(@(X, fail, fs, file) stubI_replayMessl(X, fail, fs, file, ...
+  combined_mask_dir, '', 'souden', '', 'mask', 1), ...
+  workDir, ...
+		outDir, [5 5], 1, 1, 1,'[de]t05((?!bth).)*\.CH1\.wav$'); 
+
