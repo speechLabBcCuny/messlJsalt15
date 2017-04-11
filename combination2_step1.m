6ace896939487a708afa39a185241de32299ce1f
nateanl <nizhaoheng@gmail.com>
2017-04-11 16:47:39 -0400

    fixed parameters in experiments

--- combination2_step1.m
+++ combination2_step1.m
@@ -3,22 +3,21 @@ addpath('../messl');
 addpath('../utils');
 
 %option for combination
-combination_option = 'min';
+combination_option = 'max';
 
 
 workDir = '/home/data/CHiME3/data/audio/16kHz/isolated/';
-outDir = '/scratch/near/CHiME3/lstm_messl/min/';
-lstm_dir = '/scratch/near/CHiME3/lstm_output/';
+outDir = '/scratch/near/CHiME3/fixed_model/0411_v2/max/';
+lstm_dir = '/scratch/near/CHiME3/fixed_model/v2/lstm-out-0411:14:06/';
 
 %first create messl masks in local directory
 messl_dir = '/scratch/mim/chime3/messlMcMvdrMrf.2Hard5Lbp4Slate/data/';
-
+%messl_dir = '/scratch/near/CHiME3/v3/maskhold11_lstm_initial_messl_output/data/';
 
 
 
 %then combine the masks with lstm mask
 enhance_wrapper(@(X, fail, fs, file) stubI_LSTMMessl2(X, fail, fs, file, ...
-    messl_dir, '', 'souden', 'ipd', 'mask', 1,'','',lstm_dir,combination_option), ...
-    workDir, outDir, [5 5], 1, 1, 1,'.[de]t05.*\.CH1\.wav',0);
+    messl_dir, '', 'souden', '', 'mask', 1,9,'',lstm_dir,combination_option),workDir, outDir, [5 5], 1, 1, 1,'.[de]t05((?!bth).)*\.CH1\.wav',1);
 
-fprintf('Combining LSTM with MESSL, average option');
+fprintf('Combining LSTM with MESSL, %s option', combination_option);
--- test_mask.py
+++ test_mask.py
@@ -10,13 +10,17 @@ from keras.layers import LSTM, Dense, Lambda, Merge
 from keras.layers.normalization import BatchNormalization
 from keras.layers.wrappers import TimeDistributed, Bidirectional
 import tensorflow as tf
-
+from keras.backend.tensorflow_backend import set_session
+os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+config = tf.ConfigProto()
+config.gpu_options.allow_growth=True
+set_session(tf.Session(config=config))
 base_dir = '/scratch/near/test/mask/'
-model_dir = '/home/felix/myscratch/IS2017-CHIME3-Exps/Saved Experiments/exp_2017-02-14_12:12:25/'
-save_dir = '/scratch/near/test_prediction/'
+model_dir = '/scratch/felix/IS2017-CHIME3-Exps/exp-nspect2mask/iaf/exp_2017-04-07 22:38:12/'
+save_dir = '/scratch/near/CHiME3/fixed_model/v2/lstm-out-0411:14:06/'
 data_dir = '/home/data/CHiME3/data/audio/16kHz/local/messl-spects-noisy/data/'
-test_list = mk.prep_list_for_keras(data_dir, 'dt05.*simu')
-weights_filename = "keras_model_nspect2mask_weights.h5"
+test_list = mk.prep_list_for_keras(data_dir, '[ed]t05')
+weights_filename = "iaf_model_2017-04-10 15:10:20_vl:0.576155249803.hdf5"
 
 print len(test_list)
 #load the latest model
@@ -29,20 +33,20 @@ model = Sequential()
 output_dim = 513
 feat_num = 513
 # conversion to dB  # f = lambda x: tf.log(tf.abs(x))
-model.add(TimeDistributed(Lambda(lambda x: tf.log(tf.abs(x))), input_shape = (None,feat_num)))
+#model.add(TimeDistributed(Lambda(lambda x: tf.log(tf.abs(x))), input_shape = (None,feat_num)))
 
 # normalize per feature per batch
-model.add(BatchNormalization(mode=0, axis=-1, input_shape = (None,513)))
+#model.add(BatchNormalization(mode=0, axis=-1, input_shape = (None,513)))
 
 # bidirectional lstm layer
-model.add(Bidirectional(LSTM(input_length=None, output_dim=1024, return_sequences=True)))
+#model.add(Bidirectional(LSTM(input_length=None, output_dim=1024, return_sequences=True)))
 
 # out layer
-model.add(TimeDistributed(Dense(output_dim=output_dim, activation='sigmoid')))
+#model.add(TimeDistributed(Dense(output_dim=output_dim, activation='sigmoid')))
 # for a mean squared error regression problem
-model.compile(optimizer='RMSprop',loss='mse')
+#model.compile(optimizer='RMSprop',loss='mse')
 # load weights into new model
-model.load_weights(model_dir+weights_filename)
+model=load_model(model_dir+weights_filename)
 print("Loaded model weights from disk")
 print "model loaded"
 print "......................"
@@ -53,12 +57,13 @@ for test_file in test_list:
     test_input = abs(noisy)
     test_output = model.predict(test_input)
     #combine the six masks to one single mask by taking the average.
-    mask = np.mean(test_output,axis=0)
+    mask = np.max(test_output,axis=0)
     #save the mask to mat file
     save_file = save_dir+test_file.replace(data_dir, "")
     Save_dir = os.path.split(save_file)[0]
     if not os.path.exists(Save_dir):
         os.mkdir(Save_dir)
     sio.savemat(save_file, {'mask': mask})
+    print test_file, mask.shape
 print "finish"
 print len(test_list)
